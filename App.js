b653a2895f1a4b4b93084e040c6c166f2db83e1b
mohankrishna13 <mohankrishnacigiripati@gmail.com>
2020-01-23 16:39:40 +0530

    Invoking StartingPage functionality implemented

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import StartingPage from './src/StartingPage'
+class App extends Component {
+    render() {
+        return (
+            <StartingPage  />
+        )
+    }
+}
+export default App;
