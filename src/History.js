037b4892b947a0be8ec5f94ead3641eb550b6785
mohankrishna13 <mohankrishnacigiripati@gmail.com>
2020-01-23 16:35:05 +0530

    Displaying completed words to the user functionalities

---
+++ src/History.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
+import Button from 'apsl-react-native-button';
+
+class Historypage extends Component {
+    skipWord = () => {
+        this.props.skipCurrent();
+    }
+    resetGame = (e) => {
+        this.props.resetGame();
+    }
+    setIndex = (e) => {
+        this.props.setindexToShowHistory(e);
+    }
+    render() {
+        return (
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={{ justifyContent: 'center', alignItems: 'center', margin: 5 }}>
+                        <Text style={{ fontSize: 25 }}>History</Text>
+                    </View>
+                    <Button style={styles.WordsButton} textStyle={{ fontSize: 18, color: 'white' }}
+                        onPress={() => this.skipWord()}>
+                        Get New Word
+                        </Button>
+                    <Button style={{ height: 35, width: 'auto' }} textStyle={{ fontSize: 15 }}
+                        onPress={() => this.setIndex(this.props.completedWordsForHistory.length)}>
+                        playing..{this.props.currentWordScore}
+                    </Button>
+                    {this.props.completedWordsForHistory.map((item, index) => (
+                        <Button style={{ height: 40, width: 'auto', backgroundColor: this.props.color[index] }} textStyle={{ fontSize: 15, marginLeft: 0 }}
+                            onPress={() => this.setIndex(index)}>
+                            {item}{this.props.completedWordsScore[index]}
+                        </Button>
+                    ))}
+                    <Button style={styles.WordsButton} textStyle={{ fontSize: 18, color: 'white' }}
+                        onPress={() => this.resetGame()}>
+                        Reset Game
+                    </Button>
+                </View>
+            </ScrollView>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        borderRadius: 7,
+        marginTop: 1,
+        marginLeft: 2,
+        borderColor: 'gray',
+        borderWidth: 2
+    },
+    WordsButton: {
+        height: 40,
+        width: 'auto',
+        borderRadius: 8,
+        borderColor: 'white',
+        backgroundColor: '#1e90ff',
+    }
+})
+export default Historypage;
